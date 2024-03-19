e464158786b13284b932105c2180ef638190c365
Robert C <cmrecki.robert@gmail.com>
2024-03-19 14:15:01 +0100

    Finished

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<h1>This is an about page</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste officiis mollitia voluptatibus et, veniam nobis assumenda ut blanditiis quod quisquam odit optio dignissimos nulla pariatur ad
+			dolores, eligendi asperiores, obcaecati perferendis. Nihil dicta veniam in quae beatae mollitia, ullam tempora accusantium quos. Sequi facilis dolores architecto a optio sint eaque
+			temporibus labore blanditiis explicabo animi ipsa fugiat distinctio quasi dolor consectetur, minima atque est! Dolore voluptatem adipisci aliquid, quo quibusdam et quasi fugit possimus,
+			accusamus in molestias recusandae doloribus ipsam dicta dolorem enim tempora deserunt totam tenetur sapiente minus non ullam architecto autem! Rerum vero quis adipisci nihil? Repudiandae,
+			quas.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste officiis mollitia voluptatibus et, veniam nobis assumenda ut blanditiis quod quisquam odit optio dignissimos nulla pariatur ad
+			dolores, eligendi asperiores, obcaecati perferendis. Nihil dicta veniam in quae beatae mollitia, ullam tempora accusantium quos. Sequi facilis dolores architecto a optio sint eaque
+			temporibus labore blanditiis explicabo animi ipsa fugiat distinctio quasi dolor consectetur, minima atque est! Dolore voluptatem adipisci aliquid, quo quibusdam et quasi fugit possimus,
+			accusamus in molestias recusandae doloribus ipsam dicta dolorem enim tempora deserunt totam tenetur sapiente minus non ullam architecto autem! Rerum vero quis adipisci nihil? Repudiandae,
+			quas.
+		</p>
+	</div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<h1>Homepage</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste officiis mollitia voluptatibus et, veniam nobis assumenda ut blanditiis quod quisquam odit optio dignissimos nulla pariatur ad
+			dolores, eligendi asperiores, obcaecati perferendis. Nihil dicta veniam in quae beatae mollitia, ullam tempora accusantium quos. Sequi facilis dolores architecto a optio sint eaque
+			temporibus labore blanditiis explicabo animi ipsa fugiat distinctio quasi dolor consectetur, minima atque est! Dolore voluptatem adipisci aliquid, quo quibusdam et quasi fugit possimus,
+			accusamus in molestias recusandae doloribus ipsam dicta dolorem enim tempora deserunt totam tenetur sapiente minus non ullam architecto autem! Rerum vero quis adipisci nihil? Repudiandae,
+			quas.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste officiis mollitia voluptatibus et, veniam nobis assumenda ut blanditiis quod quisquam odit optio dignissimos nulla pariatur ad
+			dolores, eligendi asperiores, obcaecati perferendis. Nihil dicta veniam in quae beatae mollitia, ullam tempora accusantium quos. Sequi facilis dolores architecto a optio sint eaque
+			temporibus labore blanditiis explicabo animi ipsa fugiat distinctio quasi dolor consectetur, minima atque est! Dolore voluptatem adipisci aliquid, quo quibusdam et quasi fugit possimus,
+			accusamus in molestias recusandae doloribus ipsam dicta dolorem enim tempora deserunt totam tenetur sapiente minus non ullam architecto autem! Rerum vero quis adipisci nihil? Repudiandae,
+			quas.
+		</p>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+	name: "HomeView",
+	components: {},
+};
 </script>
